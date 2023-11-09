d5acc532b5ddf03f0b1167bb4e21a1f729ddaaa8
eunchaekang27 <145053331+eunchaekang27@users.noreply.github.com>
2023-11-09 13:03:30 -0600

    Update Password-registration.py

--- Password-registration.py
+++ Password-registration.py
@@ -1,10 +1,12 @@
 counter = 0
 password = ""
-while counter < 8:
-  counter += 1
+while counter < 8: 
   letter = input("Enter a character of your password:")
-  if letter.isalnum():
+  if len(letter) == 1 and letter.isalnum():
+    counter += 1
     password += letter
+  elif len(letter) > 1:
+    print("Please input one character at a time")
   else:
     print("Please input a letter or a number")
 
